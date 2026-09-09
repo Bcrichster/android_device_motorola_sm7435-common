406b6006fc610d281ed61f531587b83ed924dcf1
Bcrichster <11166051+Bcrichster@users.noreply.github.com>
2026-09-09 19:31:05 -0400

    Blissify lineage power-libperfmgr path

--- common.mk
+++ common.mk
@@ -299,7 +299,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/bliss/interfaces/power-libperfmgr \
     hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client
 
